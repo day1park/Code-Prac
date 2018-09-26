821c84f05b85fb10cc4ebc163b25ee71c9b72e81
day1park <daypark2408@gmail.com>
2018-09-26 11:09:32 +1000

    loops file

---
+++ Ruby/loops.rb
@@ -0,0 +1,29 @@
+# while condition is true the loop will continue to run
+# WHILE loop
+counter = 1
+while counter < 11
+  puts counter
+  counter = counter + 1
+end
+
+i = 0
+while i < 5
+  puts i
+  i += 1
+end
+
+# UNTIL loop
+
+i = 0
+until i == 6
+  i = i + 1
+end
+puts i
+
+counter = 1
+until counter > 10
+  puts counter
+  counter += 1
+end
+
+#  '+=', '-=', '*=', '/=' <--- 'Assignment Operator'
