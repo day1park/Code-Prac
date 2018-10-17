4d688f6aed4be50ead93b2144d29ac9e73cbaf8b
day1park <daypark2408@gmail.com>
2018-10-17 15:33:18 +1100

    blocks

--- Ruby/blocks.rb
+++ Ruby/blocks.rb
@@ -104,3 +104,43 @@ def prime(n)
   puts by_three?(296) # false
   #if there's nothing remaing when divided by 3 then it is divisible by 3
 
+
+  # Blocks Are Like Nameless Methods
+  # Blocks can be defined with either the keywords do and end or with curly braces ({}).
+  1.times do
+    puts "I'm a code block!"
+  end
+  
+  1.times { puts "As am I!" }
+
+
+  # method that capitalizes a word
+def capitalize(string) 
+  puts "#{string[0].upcase}#{string[1..-1]}"
+end
+
+capitalize("ryan") # prints "Ryan"
+capitalize("jane") # prints "Jane"
+
+# block that capitalizes each string in the array
+["ryan", "jane"].each {|string| puts "#{string[0].upcase}#{string[1..-1]}"} # prints "Ryan", then "Jane"
+
+my_array = [3, 4, 8, 7, 1, 6, 5, 9, 2]
+
+# Call the sort! method on my_array below.
+# my_array should then equal [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+
+# The block, {|i| puts i}, is passed the current
+# array item each time it is evaluated. This block
+# prints the item. 
+[1, 2, 3, 4, 5].each { |i| puts i }
+
+# This block prints the number 5 for each item.
+# (It chooses to ignore the passed item, which is allowed.)
+[1, 2, 3, 4, 5].each { |i| puts 5 * i }
+
+sorted = my_array.sort!
+sorted.each do |i| 
+  puts i
+end
