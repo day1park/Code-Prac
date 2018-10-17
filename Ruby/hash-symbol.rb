e408c00f88fb984bc5e4f2dafe732242c0829faa
day1park <daypark2408@gmail.com>
2018-10-18 08:51:46 +1100

    .select method

--- Ruby/hash-symbol.rb
+++ Ruby/hash-symbol.rb
@@ -141,3 +141,110 @@ strings.each do |s|
     }
   
   puts movies[:batman]
+
+  movies = {
+    batman: "meh ok for DC",
+    spider_man: "my hero"
+    }
+  
+  puts movies[:spider_man]
+
+  
+
+# It builds two alphabet hashes: one that pairs string letters with their place in the alphabet ( "a" with 1, "b" with 2...) and one that uses symbols (:a with 1, :b with 2...)
+  require 'benchmark'
+
+string_AZ = Hash[("a".."z").to_a.zip((1..26).to_a)]
+symbol_AZ = Hash[(:a..:z).to_a.zip((1..26).to_a)]
+
+string_time = Benchmark.realtime do
+  100_000.times { string_AZ["r"] }
+end
+
+symbol_time = Benchmark.realtime do
+  100_000.times { symbol_AZ[:r] }
+end
+
+puts "String time: #{string_time} seconds."
+puts "Symbol time: #{symbol_time} seconds."
+  
+  
+
+
+grades = { alice: 100,
+    bob: 92,
+    chris: 95,
+    dave: 97
+  }
+  
+  grades.select { |name, grade| grade <  97 }
+  # ==> { :bob => 92, :chris => 95 }
+  
+  grades.select { |k, v| k == :alice }
+  # ==> { :alice => 100 }
+
+
+  movie_ratings = {
+    memento: 3,
+    primer: 3.5,
+    the_matrix: 5,
+    truman_show: 4,
+    red_dawn: 1.5,
+    skyfall: 4,
+    alex_cross: 2,
+    uhf: 1,
+    lion_king: 3.5
+  }
+  # Add your code below!
+  
+  good_movies = movie_ratings.select { |k, v| v > 3 }
+  
+  puts good_movies
+
+  movie_ratings = {
+    memento: 3,
+    primer: 3.5,
+    the_matrix: 5,
+    truman_show: 4,
+    red_dawn: 1.5,
+    skyfall: 4,
+    alex_cross: 2,
+    uhf: 1,
+    lion_king: 3.5
+  }
+  # Add your code below!
+  
+  good_movies = movie_ratings.select { |k, v| v > 3 }
+  
+  good_movies.each { |k, v| puts k.to_s }
+
+  
+
+  my_hash = { one: 1, two: 2, three: 3 }
+
+my_hash.each_key { |k| print k, " " }
+# ==> one two three
+
+my_hash.each_value { |v| print v, " " }
+# ==> 1 2 3
+
+
+movie_ratings = {
+    memento: 3,
+    primer: 3.5,
+    the_matrix: 3,
+    truman_show: 4,
+    red_dawn: 1.5,
+    skyfall: 4,
+    alex_cross: 2,
+    uhf: 1,
+    lion_king: 3.5
+  }
+  # Add your code below!
+  
+  movie_ratings.each_key{ |k| puts k }
+
+  good_movies = movie_ratings.select { | k, v| v > 3 }
+
+good_movies.each_key{ |k| print k, " "}
+good_movies.each_key{ |k| puts k}
