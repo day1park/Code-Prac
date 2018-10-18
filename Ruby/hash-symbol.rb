40bc299c3c0d842997ecd8a7c56cdf95a4753158
day1park <daypark2408@gmail.com>
2018-10-18 16:00:17 +1100

    more hash work

--- Ruby/hash-symbol.rb
+++ Ruby/hash-symbol.rb
@@ -248,3 +248,122 @@ movie_ratings = {
 
 good_movies.each_key{ |k| print k, " "}
 good_movies.each_key{ |k| puts k}
+
+
+
+movies = {
+    Memento: 3,
+    Primer: 4,
+    Ishtar: 1
+  }
+  
+  puts "What would you like to do?"
+  puts "-- Type 'add' to add a movie."
+  puts "-- Type 'update' to update a movie."
+  puts "-- Type 'display' to display all movies."
+  puts "-- Type 'delete' to delete a movie."
+  
+  choice = gets.chomp.downcase
+  case choice
+  when 'add'
+    puts "What movie do you want to add?"
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+      puts "What's the rating? (Type a number 0 to 4.)"
+      rating = gets.chomp
+      movies[title.to_sym] = rating.to_i
+      puts "#{title} has been added with a rating of #{rating}."
+    else
+      puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
+    end
+  when 'update'
+    puts "What movie do you want to update?"
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+      puts "Movie not found!"
+    else
+      puts "What's the new rating? (Type a number 0 to 4.)"
+      rating = gets.chomp
+      movies[title.to_sym] = rating.to_i
+      puts "#{title} has been updated with new rating of #{rating}."
+    end
+  when 'display'
+    movies.each do |movie, rating|
+      puts "#{movie}: #{rating}"
+    end
+  when 'delete'
+    puts "What movie do you want to delete?"
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+      puts "Movie not found!"
+    else
+      movies.delete(title.to_sym)
+      puts "#{title} has been removed."
+    end
+  else
+    puts "Sorry, I didn't understand you."
+  end
+
+
+  favorite_foods = {
+    'vegetable' => 'broccoli'
+  }
+  puts "Do you like coding in Ruby?"
+  answer = gets.chomp
+
+
+
+
+
+
+
+
+  movies = {
+    spiderman: 4,
+    bob: 4
+    }
+  
+  puts "do you want to add update delete or display?"
+  
+  choice = gets.chomp.downcase
+  
+  case choice
+    when "add"
+    puts "what movie you wanna add?"
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+        puts "what do you rate it?"
+      rating = gets.chomp
+      movies[title.to_sym] = rating.to_i
+      puts "all done mate! added #{title} with a rating of : #{rating}"
+      puts movies
+    else
+      puts "that movie #{title} rating of #{movies[title.to_sym]} exists mate! check with the display command "
+    end
+    when "update"
+    puts "what movie?!"
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+      puts "theres no movie called #{title}"
+    else
+      puts "what is the new rating for it?"
+      rating = gets.chomp.to_i
+    end
+    puts movies
+    when "display"
+      movies.each do |movie, rating| 
+        puts "#{movie}: #{rating}"
+       end
+    when "delete"
+    puts "what movie do you want to delete?"
+    title = gets.chomp
+     if movies[title.to_sym].nil?
+       puts "that movie #{title} don't exist fool!"
+     else
+       movies.delete(title.to_sym)
+       puts "#{title} has been Deleted!"
+       puts movies
+     end
+  else
+    puts "Error!"
+  end
