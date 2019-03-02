165e958afa43b5aae2002fa9cdcb91d3be2b5379
day1park <daypark2408@gmail.com>
2019-03-02 16:37:53 +1100

    add rainbows

--- JS30/12 - Key Sequence Detection/scripts.js
+++ JS30/12 - Key Sequence Detection/scripts.js
@@ -1,6 +1,13 @@
 const pressed = [];
+const secretCode = "password";
 
 window.addEventListener("keyup", e => {
   console.log(e.key);
   pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join("").includes(secretCode)) {
+    alert("PASSWORD CONFIRMED! UNLOCKING CORNIFY RAINBOW SEQUENCE!").;
+    cornify_add();
+  }
+  console.log(pressed);
 });
