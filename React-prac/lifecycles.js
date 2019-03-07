83e466557884ffad23ccab2b76a5826ef8ddec26
day1park <daypark2408@gmail.com>
2019-03-07 18:27:30 +1100

    cDM api call example

--- React-prac/lifecycles.js
+++ React-prac/lifecycles.js
@@ -4,6 +4,14 @@ class Example extends React.Component {
   }
   // this method actually gets called before the component is rendered
 
+    // method to make API calls example 
+  componentDidMount() {
+    fetch(url).then(results => {
+        // Do something with the results
+    })
+  
+
+
   render() {
     return <h1>Hello world</h1>;
   }
