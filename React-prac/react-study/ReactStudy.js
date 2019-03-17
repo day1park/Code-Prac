33ff26030f4e631ffd120136a084d01fcbd367f9
day1park <daypark2408@gmail.com>
2019-03-17 21:44:08 +1100

    add notes

--- React-prac/react-study/ReactStudy.js
+++ React-prac/react-study/ReactStudy.js
@@ -1,3 +1,10 @@
 import React from "react";
 
 // import React from React library object
+import ReactDOM from "react-dom";
+
+// every component must come from a component class.
+
+//component class, you use a base class from the React library: React.Component.
+
+// React.Component is a JavaScript class. To create your own component class, you must subclass React.Component. You can do this by using the syntax class YourComponentNameGoesHere extends React.Component {}.
