c03a832e550a2d06b44cd0c4b3530dd9a511b828
day1park <daypark2408@gmail.com>
2019-03-18 16:37:38 +1100

    add notes

--- React-prac/react-study/ReactStudy.js
+++ React-prac/react-study/ReactStudy.js
@@ -8,3 +8,9 @@ import ReactDOM from "react-dom";
 //component class, you use a base class from the React library: React.Component.
 
 // React.Component is a JavaScript class. To create your own component class, you must subclass React.Component. You can do this by using the syntax class YourComponentNameGoesHere extends React.Component {}.
+
+// React Classes are written in UpperCamelCase
+
+// To call a component’s render method, you pass that component to ReactDOM.render(). Notice your component, being passed as ReactDOM.render()‘s first argument:
+
+ReactDOM.render(<MyComponentClass />, document.getElementById("app"));
