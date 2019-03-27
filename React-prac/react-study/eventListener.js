ce6675949eba9433227aa20e81499f1f9fe8327a
day1park <daypark2408@gmail.com>
2019-03-27 17:14:08 +1100

    add changes

--- React-prac/react-study/eventListener.js
+++ React-prac/react-study/eventListener.js
@@ -1,15 +1,23 @@
-class MyClass extends React.Component {
+class Button extends React.Component {
   myFunc() {
     alert("Stop it.  Stop hovering.");
   }
+
+  scream() {
+    alert('AAAAAAAAHHH!!!!!');
 
   render() {
     return (
       <div onHover={this.myFunc}>
         <h1>this is an example</h1>
+
+        <button onClick={this.scream}>AAAAAH!</button>
       </div>
     );
   }
 }
 
+ReactDOM.render(<Button />, document.getElementById('app'))
 // Render functions often contain event listeners
+
+// the component class has two methods: .myFunc() and .render(). .myFunc() is being used as an event handler. .myFunc() will be called any time that a user hovers over the rendered <div></div>.
