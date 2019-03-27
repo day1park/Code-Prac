fdc8f19651d1668a910e676f8905e81a382e06ab
day1park <daypark2408@gmail.com>
2019-03-27 18:05:58 +1100

    add export notes

---
+++ React-prac/react-study/export.js
@@ -0,0 +1,27 @@
+export const faveManifestos = {
+  futurist: 'http://www.artype.de/Sammlung/pdf/russolo_noise.pdf',
+  agile: 'https://agilemanifesto.org/iso/en/manifesto.html',
+  cyborg:   'http://faculty.georgetown.edu/irvinem/theory/Haraway-CyborgManifesto-1.pdf'
+};
+
+
+// export multiple things from the same file:
+
+export const faveManifestos = {
+  futurist: 'http://www.artype.de/Sammlung/pdf/russolo_noise.pdf',
+  agile:  'https://agilemanifesto.org/iso/en/manifesto.html',
+  cyborg:   'http://faculty.georgetown.edu/irvinem/theory/Haraway-CyborgManifesto-1.pdf'
+};
+
+export const alsoRan = 'TimeCube';
+
+
+// In a different file, import the name of the var, let, const, function, or class from the first file:
+
+// App.js:
+
+// Import faveManifestos and alsoRan from ./Manifestos.js:
+import { faveManifestos, alsoRan } from './Manifestos';
+
+// Use faveManifestos:
+console.log(`A Cyborg Manifesto:  ${faveManifestos.cyborg}`); 
