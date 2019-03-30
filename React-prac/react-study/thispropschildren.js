ae6c2a2ed3637f1ca924b1e27e22481e8789b49a
day1park <daypark2408@gmail.com>
2019-03-30 18:41:25 +1100

    add props children notes

--- React-prac/react-study/thispropschildren.js
+++ React-prac/react-study/thispropschildren.js
@@ -21,3 +21,26 @@ export class List extends React.Component {
     );
   }
 }
+
+// where list will be rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import { List } from "./List";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <List type="Living Musician">
+          <li>Sachiko M</li>
+          <li>Harvey Sid Fisher</li>
+        </List>
+        <List type="Living Cat Musician">
+          <li>Nora the Piano Cat</li>
+        </List>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
