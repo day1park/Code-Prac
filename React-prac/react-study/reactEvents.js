5b8e7a17de8db7f0b799f29b141bc453aa046548
day1park <daypark2408@gmail.com>
2019-03-31 18:45:29 +1100

    add react event notes

--- React-prac/react-study/reactEvents.js
+++ React-prac/react-study/reactEvents.js
@@ -14,3 +14,25 @@
 <button onClick={activateLasers}>
   Activate Lasers
 </button>
+
+
+// cannot return false to prevent default behavior in React. You must call preventDefault explicitly. For example, with plain HTML, to prevent the default link behavior of opening a new page, you can write:
+<a href="#" onclick="console.log('The link was clicked.'); return false">
+  Click me
+</a>
+
+
+
+// In React, this could instead be:
+function ActionLink() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+  }
+
+  return (
+    <a href="#" onClick={handleClick}>
+      Click me
+    </a>
+  );
+}
