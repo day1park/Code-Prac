84b41a8646d1ceb4dee9ee7f59a9a699ba66f2fe
day1park <daypark2408@gmail.com>
2019-04-04 16:59:45 +1100

    add parent file

--- React-prac/react-study/siblingComponents.js
+++ React-prac/react-study/siblingComponents.js
@@ -1,1 +1,33 @@
 // A child component updates its parent’s state, and the parent passes that state to a sibling component.
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Child } from "./Child";
+import { Sibling } from "./Sibling";
+
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { name: "Frarthur" };
+
+    this.changeName = this.changeName.bind(this);
+  }
+
+  changeName(newName) {
+    this.setState({
+      name: newName
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Child name={this.state.name} onChange={this.changeName} />
+        <Sibling />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Parent />, document.getElementById("app"));
