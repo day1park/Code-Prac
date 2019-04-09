2b1e00f6f05798e185849cf59b487c625caa2233
day1park <daypark2408@gmail.com>
2019-04-09 15:08:57 +1000

    mount lifecycle methods

--- React-prac/react-study/lifecycle.js
+++ React-prac/react-study/lifecycle.js
@@ -81,3 +81,67 @@ ReactDOM.render(<Flashy color="red" />, document.getElementById("app"));
 setTimeout(() => {
   ReactDOM.render(<Flashy color="green" />, document.getElementById("app"));
 }, 2000);
+
+// The constructor method is invoked when a new component instance is created and runs before the entire component’s lifecycle starts. It runs before componentWillMount, which is the start of the mounting phase of the component.
+
+
+
+
+// componentDidMount
+
+import React from 'react';
+
+export class Example extends React.Component {
+  componentDidMount() {
+    alert('component just finished mounting!');
+  }
+
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
+
+
+// componentDidMount is a good place to connect a React app to external applications, such as web APIs or JavaScript frameworks. componentDidMount is also the place to set timers using setTimeout or setInterval.
+
+
+// Calling setState() in componentDidMount() will not end up in an infinite loop, but will just invoke an additional rendering. 
+
+
+// 2 different lifecycle methods examples
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Flashy extends React.Component {
+  componentWillMount() {
+    alert('AND NOW, FOR THE FIRST TIME EVER...  FLASHY!!!!');
+  }
+
+  componentDidMount() {
+        alert('YOU JUST WITNESSED THE DEBUT OF...  FLASHY!!!!!!!');
+  }
+  render() {
+
+    alert('Flashy is rendering!');
+
+
+
+    return (
+      <h1 style={{ color: this.props.color }}>
+        OOH LA LA LOOK AT ME I AM THE FLASHIEST
+      </h1>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Flashy color='red' />,
+  document.getElementById('app')
+);
+
+setTimeout(() => {
+  ReactDOM.render(
+    <Flashy color='green' />,
+    document.getElementById('app')
+  );
+}, 2000);
